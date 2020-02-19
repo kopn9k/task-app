597d563f0d79e12c9a137d7a8c736d7b29f6e0d3
anastasiya.piulskaya <anastasiya.piulskaya@itechart-group.com>
2020-02-19 13:39:47 +0300

    Updates

---
+++ precache-manifest.841f469a0b5d357dbddec95243d0def3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42576591cf15db697bc0d2d0386051d7",
+    "url": "/task-app/index.html"
+  },
+  {
+    "revision": "043fcc96c2f5d42bc2e9",
+    "url": "/task-app/static/css/2.d5794a55.chunk.css"
+  },
+  {
+    "revision": "043fcc96c2f5d42bc2e9",
+    "url": "/task-app/static/js/2.584a1e84.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/task-app/static/js/2.584a1e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25cc91a50fe03b5093ef",
+    "url": "/task-app/static/js/main.3f16022b.chunk.js"
+  },
+  {
+    "revision": "135c184395c0946e4369",
+    "url": "/task-app/static/js/runtime-main.aaaffb68.js"
+  }
+]);
